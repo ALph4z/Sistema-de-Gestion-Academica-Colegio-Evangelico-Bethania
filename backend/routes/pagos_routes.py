bda1a0af491d9fc10202e0623862a6cc81345b9a
Arowarlin <100679806@uasd.edu.do>
2026-09-06 11:30:14 -0500

    Update pagos_routes.py

--- backend/routes/pagos_routes.py
+++ backend/routes/pagos_routes.py
@@ -1,15 +1,25 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+
 from backend.config.database import get_db
 from backend.models.schemas import PagoCreate
 from backend.controllers import pagos_controller as ctrl
 
-router = APIRouter(prefix=”/pagos”, tags=[“Pagos”])
 
-@router.get(”/”)
+router = APIRouter(
+    prefix="/pagos",
+    tags=["Pagos"]
+)
+
+
+@router.get("/")
 def listar(db: Session = Depends(get_db)):
-return ctrl.get_pagos(db)
+    return ctrl.get_pagos(db)
 
-@router.post(”/”)
-def crear(data: PagoCreate, db: Session = Depends(get_db)):
-return ctrl.create_pago(db, data)
+
+@router.post("/")
+def crear(
+    data: PagoCreate,
+    db: Session = Depends(get_db)
+):
+    return ctrl.create_pago(db, data)
