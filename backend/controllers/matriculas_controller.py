8bbe94c3db7cd135bff69de7b0ef264671db1eea
Arowarlin <100679806@uasd.edu.do>
2026-09-06 11:25:31 -0500

    Improve formatting in matriculas_controller.py
    Refactor get_matriculas and create_matricula for better readability.

--- backend/controllers/matriculas_controller.py
+++ backend/controllers/matriculas_controller.py
@@ -1,12 +1,23 @@
 from sqlalchemy.orm import Session
+
 from backend.config.database import Matricula, to_dict
 
+
 def get_matriculas(db: Session):
-return [to_dict(m) for m in db.query(Matricula).all()]
+    return [
+        to_dict(m)
+        for m in db.query(Matricula).all()
+    ]
+
 
 def create_matricula(db: Session, data):
-nueva = Matricula(estado=“Pendiente”, **data.dict())
-db.add(nueva)
-db.commit()
-db.refresh(nueva)
-return to_dict(nueva)
+    nueva = Matricula(
+        estado="Pendiente",
+        **data.dict()
+    )
+
+    db.add(nueva)
+    db.commit()
+    db.refresh(nueva)
+
+    return to_dict(nueva)
