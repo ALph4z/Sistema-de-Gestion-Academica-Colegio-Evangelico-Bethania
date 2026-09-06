2bb9ef8fbc8f206ed4de1d9fea8dccf21b0694c0
Arowarlin <100679806@uasd.edu.do>
2026-09-06 11:21:21 -0500

    Refactor setup_cors function in cors_middleware.py
    Refactor CORS middleware setup for better readability.

--- backend/middlewares/cors_middleware.py
+++ backend/middlewares/cors_middleware.py
@@ -1,9 +1,9 @@
 from fastapi.middleware.cors import CORSMiddleware
 
 def setup_cors(app):
-app.add_middleware(
-CORSMiddleware,
-allow_origins=[”*”],  # en producción, restringe al dominio real del frontend
-allow_methods=[”*”],
-allow_headers=[”*”],
-)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
